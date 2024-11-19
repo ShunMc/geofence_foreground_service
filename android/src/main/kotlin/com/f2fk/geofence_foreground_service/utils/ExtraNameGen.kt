4235d1fb74cf2b18211b406d0c9657402991f050
Shun Matsunobu <shun.matsunobu@enechange.co.jp>
2024-11-19 14:39:13 +0900

    androidに関連するコードを削除

--- android/src/main/kotlin/com/f2fk/geofence_foreground_service/utils/ExtraNameGen.kt
+++
@@ -1,7 +0,0 @@
-package com.f2fk.geofence_foreground_service.utils
-
-import android.content.Context
-
-fun Context.extraNameGen(name: String): String {
-    return "$packageName.$name"
-}
--- example/android/app/src/main/kotlin/com/f2fk/geofence_foreground_service_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.f2fk.geofence_foreground_service_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
